7ad5590ea874fa143a2b19b953151004c647a8b3
Przemysław Buczkowski <prem@prem.moe>
2024-11-19 02:42:03 +0000

    Add a HTTP server
    This tool can now also serve the blocklist from HTTP server and update it
    every 24 hours. This is useful for clients that can auto-update
    from an HTTP server, like Transmission.

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ compatible with, for example, Transmission.
 ## Usage
 
 ```
-usage: blocklist [-h] [-gu [GZIP_URL ...]] [-c [COUNTRY ...]] [-n] [-o OUTPUT]
+usage: blocklist [-h] [-gu [GZIP_URL ...]] [-c [COUNTRY ...]] [-n] [-o OUTPUT | -s SERVE]
 
 Generate bittorrent peer IP blocklist in PeerGuardian v2 format.
+
+This tool can also serve the blocklist from HTTP server and update it
+every 24 hours. This is useful for clients that can auto-update
+from an HTTP server, like Transmission.
 
 options:
   -h, --help            show this help message and exit
@@ -19,9 +23,11 @@ options:
                         Country code or name to block
   -n, --no-compress     Don't compress the output as GZIP
   -o, --output OUTPUT   The filename to which we should write the blocklist
+  -s, --serve SERVE     Instead of writing to a file, start a HTTP server hosting the blocklist.
+                        The blocklist will be auto-updated every 24 hours.
+                        This flag takes the address to listen at in host:port format.
+                        For example, to serve on all devices on port 8080, pass --server 0.0.0.0:8000.
 ```
 
 ## Links
 - [An example blocklist you could use](https://github.com/Naunter/BT_BlockLists)
-
-
--- blocklist.py
+++ blocklist.py
@@ -1,16 +1,125 @@
 import argparse
+import datetime
+import email.utils
+import http.server
 import ipaddress
+import logging
 import gzip
+import os
 import sys
+import threading
+import time
+import typing
 
 import iso3166
 import requests
+
+# Variables shared between threads
+compressed: bool = None
+last_blocklist: bytes = None
+last_update_time: datetime.datetime = None
+next_update_time: datetime.datetime = None
+etag: bytes = None
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+# Exit program on exceptions within threads
+def custom_excepthook(args):
+    old_excepthook(args)
+    os._exit(1)  # Exit the program with an error code
+
+
+old_excepthook = threading.excepthook
+threading.excepthook = custom_excepthook
+
+
+class HttpRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        blocklist = last_blocklist
+
+        self.send_response(200)
+        self.send_header(
+            "Content-Type", "application/gzip" if compressed else "text/plain"
+        )
+        self.send_header(
+            "Content-Disposition",
+            'attachment; filename="blocklist.gz"' if compressed else "inline",
+        )
+        self.send_header(
+            "Last-Modified", email.utils.format_datetime(last_update_time)
+        )
+        next_update_secs = next_update_time - datetime.datetime.now()
+        self.send_header(
+            "Cache-Control",
+            f"max-age={int(next_update_secs.total_seconds())+10}",
+        )
+        self.send_header("Content-Length", str(len(blocklist)))
+        self.end_headers()
+
+        self.wfile.write(blocklist)
+
+    @classmethod
+    def serve(cls, host: str, port: int) -> typing.NoReturn:
+        httpd = http.server.HTTPServer((host, port), cls)
+        logger.info("Listening on %s, port %d", host, port)
+        httpd.serve_forever()
+
+
+def generate_blacklist(args: argparse.Namespace) -> None:
+    global compressed, last_blocklist, last_update_time, etag
+
+    logger.info("Started generating blocklist")
+    output = b""
+
+    for url in args.gzip_url or []:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            f = gzip.open(r.raw, mode="r")
+            output += f.read()
+
+    for country in args.country or []:
+        r = requests.get(
+            f"http://www.ipdeny.com/ipblocks/data/countries/{country.alpha2.lower()}.zone"
+        )
+        r.raise_for_status()
+        for line in r.text.splitlines():
+            network = ipaddress.ip_network(line)
+            output += f"{country.name}:{network.network_address}-{network.broadcast_address}\n".encode()
+
+    compressed = not args.no_compress
+    if args.no_compress is False:
+        output = gzip.compress(output)
+
+    last_blocklist = output
+    last_update_time = datetime.datetime.now()
+    etag = hex(hash(last_update_time))
+
+    logger.info("Finished generating blocklist")
+
+
+def update_loop(args: argparse.Namespace):
+    global next_update_time
+    while True:
+        next_update_time = datetime.datetime.now() + datetime.timedelta(days=1)
+        logger.info("Next update at %s", next_update_time)
+        time.sleep(86400)
+
+        logger.info("Starting update")
+        generate_blacklist(args)
+        logger.info("Finished update")
 
 
 def main():
     parser = argparse.ArgumentParser(
         prog="blocklist",
-        description="Generate bittorrent peer IP blocklist in PeerGuardian v2 format.",
+        description="Generate bittorrent peer IP blocklist in PeerGuardian v2 format.\n"
+        "\nThis tool can also serve the blocklist from HTTP server and update it\n"
+        "every 24 hours. This is useful for clients that can auto-update\n"
+        "from an HTTP server, like Transmission.",
+        formatter_class=argparse.RawTextHelpFormatter,
     )
 
     parser.add_argument(
@@ -34,7 +143,9 @@ def main():
         help="Don't compress the output as GZIP",
     )
 
-    parser.add_argument(
+    output_group = parser.add_mutually_exclusive_group()
+
+    output_group.add_argument(
         "-o",
         "--output",
         type=argparse.FileType("wb"),
@@ -42,13 +153,44 @@ def main():
         help="The filename to which we should write the blocklist",
     )
 
+    output_group.add_argument(
+        "-s",
+        "--serve",
+        help="Instead of writing to a file, start a HTTP server hosting the blocklist.\n"
+        "The blocklist will be auto-updated every 24 hours.\n"
+        "This flag takes the address to listen at in host:port format.\n"
+        "For example, to serve on all devices on port 8080, pass --server 0.0.0.0:8000.",
+    )
+
     args = parser.parse_args()
 
     if args.gzip_url is None and args.country is None:
+        logger.fatal(
+            "Please provide at least one source (--gzip-url and/or --country)."
+        )
         parser.print_help()
         sys.exit(1)
 
-    try:
+    if args.serve is not None:
+        host, port = args.serve.split(":")
+
+        generate_blacklist(args)
+
+        update_thread = threading.Thread(target=update_loop, args=(args,))
+        serve_thread = threading.Thread(
+            target=HttpRequestHandler.serve, args=(host, int(port))
+        )
+
+        update_thread.start()
+        serve_thread.start()
+
+        try:
+            update_thread.join()
+            serve_thread.join()
+        except KeyboardInterrupt:
+            os._exit(1)
+
+    else:
         if args.output == sys.stdout.buffer:
             print(
                 'No output file passed - writing uncompressed list to stdout. Pass "-o -" to force compression.',
@@ -56,34 +198,12 @@ def main():
             )
             args.no_compress = True
 
-        if args.no_compress is True:
-            orig_file = None
-            file = args.output
-        else:
-            orig_file = args.output
-            file = gzip.open(args.output, mode="wb")
+        generate_blacklist(args)
 
-        for url in args.gzip_url or []:
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                f = gzip.open(r.raw, mode="r")
-                for line in f:
-                    file.write(line)
-
-        for country in args.country or []:
-            r = requests.get(
-                f"http://www.ipdeny.com/ipblocks/data/countries/{country.alpha2.lower()}.zone"
-            )
-            r.raise_for_status()
-            for line in r.text.splitlines():
-                network = ipaddress.ip_network(line)
-                file.write(
-                    f"{country.name}:{network.network_address}-{network.broadcast_address}\n".encode()
-                )
-    finally:
-        file.close()
-        if orig_file is not None:
-            orig_file.close()
+        try:
+            args.output.write(last_blocklist)
+        finally:
+            args.output.close()
 
 
 if __name__ == "__main__":
